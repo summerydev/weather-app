d9944edc32fa800bb6c420c2bd95f3a4c3cc760e
summerydev <summery.dev@gmail.com>
2022-05-03 23:11:19 +0900

    🌥

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,83 +1,70 @@
-import { geolocation } from navigator;
+import { useState } from "react";
 
 export default function Home() {
-  const [now, setNow] = useState({
-    month,
-    date,
-    day,
-    hour,
-    minute,
-    second,
-  });
+  const days = new Date();
+  const [now, setNow] = useState([{}]);
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log(apiKey);
 
-  setInterval(() => {
-    const date = new Date();
-    setNow(
-      month = date.getmonth(),
-      date = date.getDate(),
-      day = date.getDay(),
-      hour = date.getHours(),
-      minute = date.getMinutes(),
-      second = date.getMilliseconds(),
-    )
-  }, 60000);
+  setNow([
+    {
+      month: days.getMonth(),
+      date: days.getDate(),
+      day: days.getDay(),
+      hour: days.getHours(),
+      minute: days.getMinutes(),
+      second: days.getMilliseconds(),
+    },
+  ]);
 
-  const [weather, setWeather] = useState({});
-  const [location, setLocation] = useState({
-    lat,
-    lon,
-  });
+  const [weather, setWeather] = useState<any[]>([{}]);
 
-  function success(pos) {
-    const { latitude, longitude } = pos.coords;
-    setLocation({
-      latitude,
-      longitude,
-    })
-    console.log(location);
+  navigator.geolocation.getCurrentPosition(success, error);
 
-    const url: string = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${process.env.REACT_APP_API_KEY}`;
+  function success(pos: any) {
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+    const url: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`;
+    console.log(url);
 
     fetch(url) //
-      .then(res => res.json()) //
-      .then(result => { //
-        setWeather();
+      .then((res) => res.json()) //
+      .then((result) => {
+        //
+        setWeather(result);
         console.log(weather);
-      })
-  };
+      });
+  }
 
-  function error(err) {
+  function error(err: any) {
     console.log(err);
     alert("sorry. Can't find you. Can't set current weather.");
   }
-
-  return
-  (<>
-    ${weather.map((data) => (
-      <p key={data.id}>
-        <div>
-          ${data.name}
-          ${data.sys.country}
-        </div>
-        ${now.map((now => (
-          <div key={now.second}>
-            ${now}
-          </div>
-        )))}
-        {/*  여기에 아이콘 들어갈 코드  */}
-        <div>
-          <span>now</span>
-          <span>${data.weather.main}</span>
-          <span>${data.main.temp - 273}℃</span>
-        </div>
-        <div>
-          <span>today</span>
-          <span>최고 ${data.main.temp_min}℃</span>
-          <span>최저 ${data.main.temp_max}℃</span>
-        </div>
-      </p>
-    ))}
-  </>)
-    ;
+  
+// fetch 아직 안했는데 map 돌려버려서 오류?
+  return (
+    <div>
+      {now && now.map((now: any) => <div key={now.second}>${now}</div>)}
+      {/*  아이콘 코드  */}
+      {weather &&
+        weather.map((data: any) => (
+          <p key={data.id}>
+            <div>
+              {data.name}
+              {data.sys.country}
+            </div>
+            <div>
+              <span>now</span>
+              <span>{data.weather.main}</span>
+              <span>{data.main.temp - 273}℃</span>
+            </div>
+            <div>
+              <span>today</span>
+              <span>최고 {data.main.temp_min - 273}℃</span>
+              <span>최저 {data.main.temp_max - 273}℃</span>
+            </div>
+          </p>
+        ))}
+    </div>
+  );
 }
-geolocation.getCurrentPosition(success, error, options);
