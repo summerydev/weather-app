c74ef90560fb6e6759f9bb84880c3c8e5bbb48c1
summerydev <summery.dev@gmail.com>
2022-05-04 21:53:58 +0900

    💫

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import Home from "./pages/Home";
-
 function App() {
-
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,70 +1,30 @@
-import { useState } from "react";
+import { weather } from "../components/NowWeather";
 
-export default function Home() {
-  const days = new Date();
-  const [now, setNow] = useState([{}]);
-  const apiKey = process.env.REACT_APP_API_KEY;
-  console.log(apiKey);
+interface ResultProps {
+  weather: weather;
+}
 
-  setNow([
-    {
-      month: days.getMonth(),
-      date: days.getDate(),
-      day: days.getDay(),
-      hour: days.getHours(),
-      minute: days.getMinutes(),
-      second: days.getMilliseconds(),
-    },
-  ]);
-
-  const [weather, setWeather] = useState<any[]>([{}]);
-
-  navigator.geolocation.getCurrentPosition(success, error);
-
-  function success(pos: any) {
-    let lat = pos.coords.latitude;
-    let lon = pos.coords.longitude;
-    const url: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`;
-    console.log(url);
-
-    fetch(url) //
-      .then((res) => res.json()) //
-      .then((result) => {
-        //
-        setWeather(result);
-        console.log(weather);
-      });
-  }
-
-  function error(err: any) {
-    console.log(err);
-    alert("sorry. Can't find you. Can't set current weather.");
-  }
-  
-// fetch 아직 안했는데 map 돌려버려서 오류?
+export default function Home({ weather }: ResultProps) {
   return (
-    <div>
-      {now && now.map((now: any) => <div key={now.second}>${now}</div>)}
-      {/*  아이콘 코드  */}
-      {weather &&
-        weather.map((data: any) => (
-          <p key={data.id}>
-            <div>
-              {data.name}
-              {data.sys.country}
-            </div>
-            <div>
-              <span>now</span>
-              <span>{data.weather.main}</span>
-              <span>{data.main.temp - 273}℃</span>
-            </div>
-            <div>
-              <span>today</span>
-              <span>최고 {data.main.temp_min - 273}℃</span>
-              <span>최저 {data.main.temp_max - 273}℃</span>
-            </div>
-          </p>
-        ))}
-    </div>
+    <>
+      {weather && (
+        <p key={weather.id}>
+          <div>
+            {weather.name}
+            {weather.sys.country}
+          </div>
+          <div>
+            <span>now</span>
+            <span>{weather.weather.main}</span>
+            <span>{weather.main.temp - 273}℃</span>
+          </div>
+          <div>
+            <span>today</span>
+            <span>최고 {weather.main.temp_min - 273}℃</span>
+            <span>최저 {weather.main.temp_max - 273}℃</span>
+          </div>
+        </p>
+      )}
+    </>
   );
 }
