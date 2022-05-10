473255e3ed6dd2199d67939b50d5593e044f4233
summer <summery.dev@gmail.com>
2022-05-11 02:17:59 +0900

    useEffect 사용, styled-commponent 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,86 @@
-import { weather } from "../components/NowWeather";
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
 
-interface ResultProps {
-  weather: weather;
+interface Weather {
+  id: any;
+  name: string;
+  weather: any;
+  main: any;
+  sys: any;
 }
+const apiKey = process.env.REACT_APP_API_KEY;
 
-export default function Home({ weather }: ResultProps) {
+export default function Home() {
+  // console.log(apiKey);
+
+  // 초기값이 없어서 에러 -> 1. 초기값 부여 2. 없을 때를 제어
+  const [weather, setWeather] = useState<Weather>();
+
+  // useEffect : dom 준비된 후에 1번만 실행, []디펜던시 부여
+  useEffect(() => {
+    function success(pos: any) {
+      let lat = pos.coords.latitude;
+      let lon = pos.coords.longitude;
+      const url: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      console.log(url);
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          console.log(weather);
+        });
+    }
+
+    function error(err: any) {
+      console.log(err);
+      alert("sorry. Can't find you. Can't set current weather.");
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
   return (
-    <>
-      {weather && (
-        <p key={weather.id}>
+    <Card>
+      {weather && weather.id ? (
+        <div key={weather.id}>
           <div>
-            {weather.name}
-            {weather.sys.country}
+            <h2>
+              {weather.name} {weather.sys.country}
+            </h2>
           </div>
+<br />
           <div>
-            <span>now</span>
-            <span>{weather.weather.main}</span>
-            <span>{weather.main.temp - 273}℃</span>
+            <h1>Now {Math.floor(weather.main.temp - 273)}℃</h1>
+            <span>{weather.weather[0].main}</span>
           </div>
-          <div>
-            <span>today</span>
-            <span>최고 {weather.main.temp_min - 273}℃</span>
-            <span>최저 {weather.main.temp_max - 273}℃</span>
-          </div>
-        </p>
+          <Sun>
+            <span>
+              ☀️ {new Date(weather.sys.sunrise * 1000).toLocaleTimeString("kr")}
+            </span>{" "}
+            <span>
+              🌙 {new Date(weather.sys.sunset * 1000).toLocaleTimeString("kr")}
+            </span>
+          </Sun>
+        </div>
+      ) : (
+        <>Error or Loading</>
       )}
-    </>
+    </Card>
   );
 }
+
+const Card = styled.div`
+  display: block;
+  text-align: center;
+  border-radius: 10px;
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  padding: 10px;
+  margin: auto;
+`;
+
+const Sun = styled.div`
+  position: relative;
+  top: 80px;
+`;
